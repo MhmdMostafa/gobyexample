9e91f9528230954fa41f0d4ee54461c5fa18b125
ProWily <wiliamyi@outlook.sa>
2021-10-24 16:39:49 +0300

    use templats

--- main.go
+++ main.go
@@ -1,13 +1,70 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/ProWily/gobyexample/bcserver"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+
+	p, _ := loadPage(title)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	t, _ := template.ParseFiles("static/edit.html")
+	t.Execute(w, p)
+}
+
+var protFlag string
+
+func init() {
+	flag.StringVar(&protFlag, "p", ":8080", "p Setup the port")
+}
+
 func main() {
-
-	fmt.Println("hello")
-	bcserver.Server()
-
+	flag.Parse()
+	test := "hello world"
+	fmt.Print(test[len("hello "):])
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	log.Fatal(http.ListenAndServe(protFlag, nil))
 }
